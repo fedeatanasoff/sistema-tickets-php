c8e85d23d7e3cdea3d83e9fac4bb9daef14a32f7
fedeatanasoff <fede.atanasoff@gmail.com>
2018-12-06 18:26:32 -0300

    prueba de conexion de base de datos

--- app.php
+++ app.php
@@ -3,3 +3,10 @@
 require_once './app/config.php';
 require_once './app/config.php';
 require_once './app/send_mail.php';
+
+function obtener_cupo($actividad_id)
+{
+	$sql = "SELECT a.actividad_id, a.bloque, a.disciplina, a.horario, a.cupo (SELECT COUNT(*) FROM registros AS r WHERE r.actividad = a.actividad_id) AS registrados FROM actividades AS a WHERE a.actividad_id = ?";
+
+	$data = array( $actividad_id );
+}
