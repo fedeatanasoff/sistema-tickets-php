91fea3ebd47ea4abd06f62a164dd040ad1058996
fedeatanasoff <fede.atanasoff@gmail.com>
2018-12-07 19:02:16 -0300

    refactorizacion menor

--- app/config.php
+++ app/config.php
@@ -3,8 +3,6 @@
 define('ENV', 'dev');
 define('DB', array(
     'dsn' => (ENV === 'dev') ? 'mysql:host=localhost;dbname=edtickets' : '';
-	'user' => (ENV === 'dev') ? 'root' : '';
-	'password' => (ENV === 'dev') ? '' : '';	
-))
-
-console.log
+'user' => (ENV === 'dev') ? 'root' : '';
+'password' => (ENV === 'dev') ? '' : '';
+));
